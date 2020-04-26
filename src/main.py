e10a4cefe5bbb6ce81aedd77504d34304151ad6d
Derik Oliveira <derikoliveira03@gmail.com>
2020-04-26 16:00:38 -0300

    Adicionando função que troca para o diretório base
    Utilizando essa função para poder rodar o script de qualquer lugar

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
-from os import listdir
+import os
 
 from conexao_mysql import MySQL
 from importacao import Importacao
 
 
 def main():
+    trocar_diretorio()
     despesas_arqs = retorna_caminhos_arqs_despesas()
     orcamento = '../dados/2019_OrcamentoDespesa.zip.csv'
     mysql_obj = MySQL('localhost', 'alunos', 'alunos', 'atividade02')
@@ -15,7 +16,15 @@ def main():
 
 
 def retorna_caminhos_arqs_despesas():
-    return ['../dados/' + arq for arq in listdir('../dados') if arq.endswith('Despesas.csv')]
+    return ['../dados/' + arq for arq in os.listdir('../dados') if arq.endswith('Despesas.csv')]
+
+
+def trocar_diretorio():
+    """Troca para o diretório desse arquivo, não importa de onde ele seja chamado"""
+    # Retirado de https://stackoverflow.com/a/1432949/12032210
+    abspath = os.path.abspath(__file__)
+    dname = os.path.dirname(abspath)
+    os.chdir(dname)
 
 
 if __name__ == '__main__':
